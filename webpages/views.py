ddedd28c93c41edf35537f45a2aa188f646d9ab4
Pranav Arora <aurorapranav187@gmail.com>
2021-12-07 09:59:33 +0530

    Pagination added

--- webpages/views.py
+++ webpages/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from .models import Post
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 # Create your views here.
 def home(request):
     posts = Post.objects.order_by("-created_date")
 
+    p = Paginator(posts, 4)
+
+    page_number = request.GET.get('page')
+    try:
+        page_obj = p.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = p.page(1)
+    except EmptyPage:
+        page_obj = p.page(p.num_pages)
+
     data = {
         'home_page': "active",
         'home_posts': posts,
+        'page_obj': page_obj,
+        'no_of_pages': p.page_range,
     }
     return render(request, 'webpages/home.html', data)
 
