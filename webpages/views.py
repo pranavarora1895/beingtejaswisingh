62f1a171c074fadf06244af8153e1477249520fa
Pranav Arora <aurorapranav187@gmail.com>
2021-12-07 09:21:39 +0530

    Fetched posts on home_page

--- webpages/views.py
+++ webpages/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 def home(request):
-    data = {'home_page': "active"}
+    posts = Post.objects.order_by("-created_date")
+
+    data = {
+        'home_page': "active",
+        'home_posts': posts,
+    }
     return render(request, 'webpages/home.html', data)
 
 def about(request):
