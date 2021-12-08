3cedecd9468d46913381e46968b5836988d8458f
Pranav Arora <aurorapranav187@gmail.com>
2021-12-08 16:03:13 +0530

    Homepage comment fixed

--- comments/models.py
+++ comments/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 class Comment(models.Model):
 
-    post_title = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post_title = models.ForeignKey(Post, related_name="post", null=True ,on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
     comment = models.TextField()
